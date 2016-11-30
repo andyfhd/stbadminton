216f74cc0e89303f394f6612a0c6c048a72cbd9d
Haidong Fu <andyfhd@gmail.com>
2016-11-30 19:16:28 +0800

    add EJS

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+
+var app = express();
+
+
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+var server = app.listen(8080, function () {
+    console.log('server is running at %s', server.address().port);
+});
